a2e08d89467b7dbd3959ee758bbad535ae7c924d
HiepTang <tpthiep@gmail.com>
2015-08-20 12:14:36 +0700

    Create MainMenu partial view, Layout

---
+++ AmeCaseBookOrg/Controllers/MenuController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using AmeCaseBookOrg.Service;
+using AmeCaseBookOrg.Models;
+
+namespace AmeCaseBookOrg.Controllers
+{
+    public class MenuController : Controller
+    {
+        private readonly ICategoryService categoryService;
+
+        public MenuController(ICategoryService categoryService)
+        {
+            this.categoryService = categoryService;
+        }
+
+        // GET: Menu
+        public PartialViewResult MainMenu()
+        {
+            var mainMenus = categoryService.GetMainMenus();
+            return PartialView(mainMenus);
+        }
+    }
+}
